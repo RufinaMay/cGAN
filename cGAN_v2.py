image normalization fixed

cGAN_v2.py
@@ -1,13 +1,11 @@
 import numpy as np
 from keras.optimizers import Adam
-from keras.layers import Input, Flatten, LeakyReLU, Dense, BatchNormalization, Reshape
+from keras.layers import Input, LeakyReLU, BatchNormalization, Reshape
 from keras.models import Model, Sequential
-from keras.datasets import mnist
 from matplotlib import pyplot as plt
 import cv2
-from keras.layers import Dense, Conv2D, Conv2DTranspose, LeakyReLU,ReLU, Dropout, Input
+from keras.layers import Conv2D, Conv2DTranspose, ReLU, Dropout
 import pickle
-from matplotlib import pyplot as plt
 
 class GAN():
     def __init__(self):
@@ -17,11 +15,11 @@ class GAN():
 #         self.IMAGE_TO_TEST = cv2.imread(r'C:\Users\Rufina\Desktop\thesis\cGAN\data\Image\00000850\day\20151101_165511.jpg')
 #         self.IMAGE_TO_TEST = cv2.resize(self.IMAGE_TO_TEST, (self.IM_SIZE,self.IM_SIZE))
 #         self.DATA_FOLDER = r'C:\Users\Rufina\Desktop\thesis\cGAN\data\Image\\'
-
+       
         self.DATA_FOLDER = '/content/gdrive/My Drive/Colab Notebooks/THESIS/cGAN/data/Image/'
-        self.IMAGE_TO_TEST = cv.imread(f'{DATA_FOLDER}00000850/day/20151101_165511.jpg')
-        self.IMAGE_TO_TEST = cv.resize(IMAGE_TO_TEST, (IM_SIZE, IM_SIZE))
-        
+        self.IMAGE_TO_TEST = cv2.imread(f'{self.DATA_FOLDER}00000850/day/20151101_165511.jpg')
+        self.IMAGE_TO_TEST = cv2.resize(self.IMAGE_TO_TEST, (self.IM_SIZE, self.IM_SIZE))
+    
         with open('day_paths.pickle', 'rb') as f:
             self.DAY_PATH = pickle.load(f)
         with open('night_paths.pickle', 'rb') as f:
@@ -33,25 +31,19 @@ class GAN():
         # Build and compile the discriminator
         self.discriminator = self.Discriminator()
         self.discriminator.compile(loss='binary_crossentropy', 
-            optimizer=optimizer,
-            metrics=['accuracy'])
+            optimizer=optimizer, metrics=['accuracy'])
 
         # Build and compile the generator
         self.generator = self.Generator()
         self.generator.compile(loss='binary_crossentropy', optimizer=optimizer)
 
-        # The generator takes noise as input and generated imgs
         day = Input(shape=self.IM_SHAPE)
         night = self.generator(day)
 
-        # For the combined model we will only train the generator
         self.discriminator.trainable = False
 
-        # The valid takes generated images as input and determines validity
         valid = self.discriminator(night)
 
-        # The combined model  (stacked generator and discriminator) takes
-        # noise as input => generates images => determines validity 
         self.combined = Model(day, valid)
         self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
 
@@ -127,7 +119,9 @@ class GAN():
         G.add(Dropout(0.5))
         
         G.add(Conv2DTranspose(filters=3, kernel_size=2, strides=(2,2), activation='tanh'))
+        G.summary()
         
+  
         day = Input(shape=self.IM_SHAPE)
         night = G(day)
     
@@ -136,7 +130,7 @@ class GAN():
     def Discriminator(self):
         D = Sequential()
         #ENCODER PART
-        D.add( Conv2D(filters=64, kernel_size=4, strides=(2,2), input_shape=(256,256,3)))
+        D.add( Conv2D(filters=64, kernel_size=4, strides=(2,2), input_shape=(self.IM_SIZE,self.IM_SIZE,3)))
         D.add(LeakyReLU(0.2))
         
         D.add( Conv2D(filters=128, kernel_size=4, strides=(2,2)))
@@ -161,12 +155,22 @@ class GAN():
         
         D.add( Conv2D(filters=1, kernel_size=2, strides=(2,2), activation='sigmoid')) 
         D.add(Reshape((-1,)))
-        
+        D.summary()
         image = Input(shape=self.IM_SHAPE)
         validity = D(image)
     
         return Model(image, validity)
-
+    
+    def image_normalization_mapping(self, image, from_min, from_max, to_min, to_max):
+      """
+      Map data from any interval [from_min, from_max] --> [to_min, to_max]
+      Used to normalize and denormalize images
+      """
+      from_range = from_max - from_min
+      to_range = to_max - to_min
+      scaled = np.array((image - from_min) / float(from_range), dtype=float)
+      return to_min + (scaled * to_range)
+    
     def Batch(self, day_paths, night_paths):
          N = len(day_paths)
          for i in range(N):
@@ -183,7 +187,8 @@ class GAN():
             #SGD
             for d, n in self.Batch(self.DAY_PATH, self.NIGHT_PATH):
                    #day, night = d/255, n/255
-                   day, night = (d - 127.5) / 127.5, (n - 127.5) / 127.5
+                   day = self.image_normalization_mapping(d, 0, 255, -1, 1)
+                   night = self.image_normalization_mapping(n, 0, 255, -1, 1)
                    gen_imgs = self.generator.predict(day[np.newaxis,:])
                    d_loss_real = self.discriminator.train_on_batch(night[np.newaxis,:], np.ones((1,1)))
                    d_loss_fake = self.discriminator.train_on_batch(gen_imgs, np.zeros((1,1)))
@@ -194,16 +199,14 @@ class GAN():
                    G_LOSS += g_loss
             print(f'epoch: {epoch}, D_LOSS: {D_LOSS/N}, G_LOSS: {G_LOSS} ')
             
-#             if epoch%10== 0:
-            img = self.generator.predict(self.IMAGE_TO_TEST[np.newaxis,:])
-            cv2.imwrite(f'{epoch}.jpg',img[0]* 127.5+ 127.5)
+            print("okay")
+            img = self.image_normalization_mapping(self.IMAGE_TO_TEST, 0, 255, -1, 1)
+            img = self.generator.predict(img[np.newaxis,:])
+            img = self.image_normalization_mapping(img[0], -1, 1, 0, 255).astype('uint8')
+            plt.imshow(img)
+            plt.show()
             
-        img = self.generator.predict(self.IMAGE_TO_TEST[np.newaxis,:])
-        plt.imshow(img[0]* 127.5+ 127.5)
-             
-               
-
-
+        
 if __name__ == '__main__':
     gan = GAN()
     gan.train(epochs=500)